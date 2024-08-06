89f0fdc1e27d47d1690c40615f22efcae971aa36
maymichaeli <maushe55@gmail.com>
2024-08-06 22:06:14 +0300

    created the tile object and the constractor

--- test/Tile.java
+++ test/Tile.java
@@ -2,6 +2,13 @@ package test;
 
 
 public class Tile {
+    public final char letter;
+    public final int score;
 
+    private Tile (char letter, int score)
+    {
+        this.letter=letter;
+        this.score= score;
+    }
 	
 }
