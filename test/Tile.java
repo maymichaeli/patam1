64e8dc9e8dc37d556084cd56f99d70e74ba997b9
maymichaeli <maushe55@gmail.com>
2024-08-07 07:42:59 +0300

    add the equals and hashCode methods

--- test/Tile.java
+++ test/Tile.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.Objects;
 
 public class Tile {
     public final char letter;
@@ -9,6 +10,19 @@ public class Tile {
     {
         this.letter=letter;
         this.score= score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return letter == tile.letter && score == tile.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(letter, score);
     }
 	
 }
