e628507782a0874a5b1ef08c4094c36d55d53758
maymichaeli <maushe55@gmail.com>
2024-08-18 21:03:33 +0300

    open for debug

--- test/MainTrain.java
+++ test/MainTrain.java
@@ -100,7 +100,7 @@ public class MainTrain {
 
 	public static void main(String[] args) {
 		testBag(); // 30 points
-		//testBoard(); // 70 points
+		testBoard(); // 70 points
 		System.out.println("done");				
 	}
 
