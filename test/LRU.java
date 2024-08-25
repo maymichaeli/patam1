f2af49546717fb214c2e8e11fc90120ddf06ba5f
maymichaeli <maushe55@gmail.com>
2024-08-25 16:43:49 +0300

    LRU func

--- test/LRU.java
+++ test/LRU.java
@@ -1,6 +1,24 @@
 package test;
 
 
-public class LRU {
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class LRU implements CacheReplacementPolicy {
+    HashSet<String> cache=new HashSet<>();
+    @Override
+    public void add(String word) {
+        if(cache.contains(word))
+            cache.remove(word);
+        else cache.add(word);
+    }
+
+    @Override
+    public String remove() {
+        String[] stringsArr=new String[cache.size()];
+        stringsArr=cache.toArray(stringsArr);
+        return stringsArr[0];
+    }
 
 }
