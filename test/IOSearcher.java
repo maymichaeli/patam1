f6c36251a57475af7d2ece9cd606e7cc401dd696
maymichaeli <maushe55@gmail.com>
2024-08-26 07:29:42 +0300

    create the func that gets a file and search the word in the file if the bloom filter doesnt work

--- test/IOSearcher.java
+++ test/IOSearcher.java
@@ -1,6 +1,25 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class IOSearcher {
-
+   public static boolean search(String word, String... fileNames) {
+        for (String fileName : fileNames) {
+            //try-with-resources => insure that the resources that we open will be close at the end
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains(word)) {
+                        return true;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
 }
